dfe91f65fd650d3f5a4c620835e5b04834c79b93
cristianbgp <cristian.granda.pastor@gmail.com>
2026-04-19 23:11:34 -0500

    Fix cambiomundial

--- src/index.ts
+++ src/index.ts
@@ -96,12 +96,12 @@ async function getAllData(sort: "buy" | "sell" = "buy") {
       accessorToSell: ([data]) => Number(data.MontoVenta),
       pageUrl: "https://chapacambio.com/",
     }),
-    // fetchHTMLAndExtract({
-    //   url: "https://www.cambiomundial.com/appcm/tpc/tipocambio/index",
-    //   querySelectorToBuy: "#txtValorCompra",
-    //   querySelectorToSell: "#txtValorVenta",
-    //   pageUrl: "https://www.cambiomundial.com"
-    // })
+    getData({
+      url: "https://www.cambiomundial.com/backend/tasaCambio/daily",
+      accessorToBuy: ([data]) => Number(data.buy),
+      accessorToSell: ([data]) => Number(data.sell),
+      pageUrl: "https://www.cambiomundial.com",
+    }),
   ]);
   const dollar = {} as Record<string, DataResult | undefined>;
   [
